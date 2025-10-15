39ba9367a5f13d773a404b030146fb5c19555ce2
Putrizahrahramadhani <putrizahraramadhani532@gmail.com>
2025-10-15 22:09:04 +0700

    perubahannn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,20 +11,20 @@ import {
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-purple-900 via-indigo-900 to-black text-white flex flex-col items-center justify-center px-6 py-12">
-      
+
       
       {/* FOTO */}
       <Image
         src="/profil_portofolio.jpeg"
         alt="Profile"
-        width={140}
-        height={140}
+        width={160}
+        height={150}
         className="rounded-full mb-6 border-4 border-white/20 shadow-xl hover:scale-105 transition-transform duration-300"
       />
 
       {/* HERO */}
       <h1 className="text-4xl md:text-5xl font-bold mb-3 bg-gradient-to-r from-purple-300 to-pink-400 bg-clip-text text-transparent">
-        Hi, Im Putri Zahrah Ramadhani
+        🌷 Hi, Im Putri Zahrah Ramadhani 🌷
       </h1>
       <p className="text-gray-300 text-center max-w-md mb-12">
         An Informatics Student and Web Developer who loves creating modern, interactive web experiences.
@@ -50,7 +50,7 @@ export default function Home() {
 
       {/* CONTACT */}
       <h2 className="text-2xl font-semibold mb-3">🌻Contact Me🌻</h2>
-      <p className="text-gray-400 mb-4">Let’s collaborate and build something amazingtogether!</p>
+      <p className="text-gray-400 mb-4">Let’s collaborate and build something amazing together!</p>
       <a
         href="mailto:example@email.com"
         className="px-6 py-3 bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg font-medium hover:opacity-90 transition"
